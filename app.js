5af11c6d00564ad0a7ad900ccb51c00ef2b7ae13
Megan Sleezer <64713309+mslee001@users.noreply.github.com>
2020-09-19 22:04:31 -0700

    Added validation for the inquirer questions

--- app.js
+++ app.js
@@ -15,44 +15,98 @@ const engineerQuestions = [
     {
         type: "input",
         message: "What is the engineer's name?",
-        name: "name"
+        name: "name",
+        validate: function(answer) {
+            if (answer !== "") {
+                return true;
+            }
+            return "Please enter at least one character";
+        }
     },
     {
         type: "input",
         message: "What is the engineer's ID?",
-        name: "id"
+        name: "id",
+        validate: function(answer) {
+            if (answer !== "") {
+                return true;
+            }
+            return "Please enter at least one character";
+        }
     },
     {
         type: "input",
         message: "What is the engineer's email?",
-        name: "email"
+        name: "email",
+        validate: function (email) {
+            valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+            if (valid) {
+                return true;
+            } else {
+                console.log(".  Please enter a valid email")
+                return false;
+            }
+        }
     },
     {
         type: "input",
         message: "What is the engineer's github user?",
-        name: "github"
+        name: "github",
+        validate: function(answer) {
+            if (answer !== "") {
+                return true;
+            }
+            return "Please enter at least one character";
+        }
     }    
 ];
 const internQuestions = [
     {
         type: "input",
         message: "What is the intern's name?",
-        name: "internName"
+        name: "internName",
+        validate: function(answer) {
+            if (answer !== "") {
+                return true;
+            }
+            return "Please enter at least one character";
+        }
     },
     {
         type: "input",
         message: "What is the intern's ID?",
-        name: "internId"
+        name: "internId",
+        validate: function(answer) {
+            if (answer !== "") {
+                return true;
+            }
+            return "Please enter at least one character";
+        }
     },
     {
         type: "input",
         message: "What is the intern's email?",
-        name: "internEmail"
+        name: "internEmail",
+        validate: function (email) {
+            valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+            if (valid) {
+                return true;
+            } else {
+                console.log(".  Please enter a valid email")
+                return false;
+            }
+        }
     },
     {
         type: "input",
         message: "What school is the intern going to?",
-        name: "internSchool"
+        name: "internSchool",
+        validate: function(answer) {
+            if (answer !== "") {
+                return true;
+            }
+            return "Please enter at least one character";
+        }
     }
 ];
 const managerQuestions = [
@@ -70,17 +124,41 @@ const managerQuestions = [
     {
         type: "input",
         message: "What is your manager's ID?",
-        name: "managerId"
+        name: "managerId",
+        validate: function(answer) {
+            if (answer !== "") {
+                return true;
+            }
+            return "Please enter at least one character";
+        }
     },
     {
         type: "input",
         message: "What is your manager's email?",
-        name: "managerEmail"
+        name: "managerEmail",
+        validate: function (email) {
+            valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+            if (valid) {
+                return true;
+            } else {
+                console.log(".  Please enter a valid email")
+                return false;
+            }
+        }
     },
     {
         type: "input",
         message: "What is your manager's office number?",
-        name: "managerOfficeId"
+        name: "managerOfficeId",
+        validate: function (value) {
+            var pass = value.match(
+              /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i
+            );
+            if (pass) {
+              return true;
+            }
+            return 'Please enter a valid phone number';
+          }
     }
 ];
 
